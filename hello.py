c4f92f2d44f91ac927eb20637e1c03e797cfc662
sosnowskijakub <sosenka1198@gmail.com>
2023-12-10 19:01:05 +0100

    first commit

---
+++ hello.py
@@ -0,0 +1,9 @@
+name = "Jakub"
+surname = "Sosnowski"
+greeting = name + " " + surname
+#print(greeting)
+
+hello = "Hello Bitch"
+my_name = input("What's your name bitch?\n")
+greetings = hello + " " + my_name
+print(greetings)
